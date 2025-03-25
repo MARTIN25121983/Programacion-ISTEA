49df5ae1dd22d8cb1524c7e12e0b77f93b15bdf9
Martin <mario.terragno@hotmail.com>
2025-03-25 16:52:45 -0300

    Update Unidad2.py

--- Unidad2.py
+++ Unidad2.py
@@ -7,5 +7,11 @@ un comentario es un texto que comienza con el símbolo # (numeral o almohadilla)
 En cambio si queremos agregar un comentario de varias líneas debemos comenzar con comillas, y luego terminar con comillas
 """
 
+##Carácter especial: \n
+##La barra invertida (\) se usa para incluir caracteres especiales. Por ejemplo, \n indica un salto de línea:
+
+
+##Ejemplo
+print("Hola Mundo\nHola Python") 
 
 
