090f979976e2b1555dde2970d0018418195b56e9
Martin <mario.terragno@hotmail.com>
2025-03-25 16:43:31 -0300

    Update Unidad2.py

--- Unidad2.py
+++ Unidad2.py
@@ -1,5 +1,11 @@
 ##Unidad 2 TIPOS DATOS | OPERADORES | VARIABLES | COMENTARIOS
 
 
+"""Comentarios -- Son notas, documentacion para darnos una ayuda en un futuro o para que otra persona sepa como esta armado.
+
+un comentario es un texto que comienza con el símbolo # (numeral o almohadilla) y se extiende hasta el final de la línea.
+En cambio si queremos agregar un comentario de varias líneas debemos comenzar con comillas, y luego terminar con comillas
+"""
+
 
 
