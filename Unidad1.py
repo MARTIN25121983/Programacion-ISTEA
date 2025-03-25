748f9b6f388dff4480f371cbff7cb045a7e76ec5
Martin <mario.terragno@hotmail.com>
2025-03-25 16:47:54 -0300

    Update Unidad1.py

--- Unidad1.py
+++ Unidad1.py
@@ -1,3 +1,15 @@
 ##Unidad 1## 
 ##Funcion Print ##Imprimir en pantalla
+"""La función print() es una de las más utilizadas en Python. Se usa para mostrar información en la consola, como mensajes o resultados de cálculos.
+La sintaxis básica es:
+print(argumento1, argumento2, ...)
+
+Cómo funciona print()
+Cuando ejecutamos print(), Python realiza varios pasos:
+
+Comprueba que el nombre print es válido.
+Verifica los argumentos que le pasaste. Si algo no coincide, muestra un error.
+Ejecuta la función, transforma los argumentos en texto comprensible y los muestra en pantalla.
+"""
+
 print("Hola Mundo") 
