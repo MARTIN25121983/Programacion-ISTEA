351842fbd56d08d893ce566246c7443891728f76
Martin <mario.terragno@hotmail.com>
2025-03-26 22:20:21 -0300

    Update Unidad3.py

---
+++ Unidad3.py
@@ -0,0 +1,6 @@
+##Unidad3 STRINGS | COMPARADORES | CONDICIONALES 
+
+variable_uno ="Hola"
+variable_dos ="Mundo"
+resultado= variable_uno + " " + variable_dos ## " " nos hace un espacio
+print(resultado)
