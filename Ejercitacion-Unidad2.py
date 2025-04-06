8402bba4d08c1180d71d09042deaf232d8f1d3fd
Martin <mario.terragno@hotmail.com>
2025-04-06 01:51:40 -0300

    Update Ejercitacion-Unidad2.py

--- Ejercitacion-Unidad2.py
+++ Ejercitacion-Unidad2.py
@@ -17,7 +17,6 @@ print(type(informacion_personal))
 
 # 4. Calcula el área de un círculo con radio 4. Imprime el resultado.
 ##Area de un circulo = 3.14 * r **2
-
 pi = 3.14
 radio= 4
 resultado= pi * (radio ** 2)
@@ -43,18 +42,30 @@ print("El resultado de la operacion compleja es" , operacion_compleja)
 print(type(operacion_compleja))
 
 # 7. Crea una variable booleana que represente si un alumno ha aprobado o no un examen y luego imprime su estado.
-
 nota = 8
 aprobado = nota >=6
 print (aprobado)
 
 # 8. Calcula el perímetro de un triángulo equilátero con lados de longitud 6. Imprime el resultado.
-
 #Perimetro del triangulo es = A + B + C
 lado = 6
 perimetro_triangulo = 3 * 6
 print("El perimetro del triangulo equilatero es" , perimetro_triangulo)
 
 # 9. Pide al usuario que ingrese su nombre, edad y ciudad de residencia y luego imprime cada uno de esos datos con su respectivo tipo de dato.
+nombre = input("Ingrese su nombre ")
+edad = input("Ingrese su edad ")
+ciudad_de_residencia = input("Ingrese su ciudad de residencia ")
+
+print(type(nombre))
+print("El nombre es: " , nombre)
+print(type(edad))
+print("La edad es: " , edad)
+print(type(ciudad_de_residencia))
+print("La ciudad de residencia es: " , ciudad_de_residencia)
+
 
 # 10. Realiza una operación matemática que involucre paréntesis, multiplicación, suma y resta. Guarda el resultado en una variable y luego imprímela junto con su tipo de dato.
+operacion_matematica = (3*2) + (4-2)
+print(type(operacion_matematica))
+print("El resultado es:" , operacion_matematica)
