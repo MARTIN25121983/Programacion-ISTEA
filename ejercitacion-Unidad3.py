fa84ba55ff32ca2b3d38860e215929e9c3e365e7
Martin <mario.terragno@hotmail.com>
2025-04-06 03:27:47 -0300

    Update ejercitacion-Unidad3.py

--- ejercitacion-Unidad3.py
+++ ejercitacion-Unidad3.py
@@ -166,7 +166,7 @@ elif IMC >=30 <=34.99:
 elif IMC >=35 <=40:
     print("Obeso Tipo II")
 else:
-	print("Obeso Tipo III")
+    print("Obeso Tipo III")
     
 #13.Conversión de unidades
 #Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit. La fórmula de conversión es `F = C * 9/5 + 32`. Pide al usuario que ingrese una temperatura en Celsius y muestra el resultado en Fahrenheit.
