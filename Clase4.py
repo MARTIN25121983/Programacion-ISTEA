e3fcf37f45de1f1409bb34445565fa250fae9bc4
Martin <mario.terragno@hotmail.com>
2025-04-14 19:15:41 -0300

    Update Clase4.py

---
+++ Clase4.py
@@ -0,0 +1,8 @@
+##While Bucle. con break salimos del bucle
+
+contador = 10
+
+while contador > 0:
+    print("Estoy dentro del bucle while, quedan ", contador, "iteraciones")
+    contador -= 1
+print("Fin del programa")
