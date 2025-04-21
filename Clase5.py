81f32356d284f144935720b1d7ce19cfac611392
Martin <mario.terragno@hotmail.com>
2025-04-21 19:30:21 -0300

    Update Clase5.py

---
+++ Clase5.py
@@ -0,0 +1,7 @@
+##Funciones (Unidad 6) y listas
+##Funciones Bloque codigo para resolver algo especifico
+#Ejemplos
+
+def mi_primer_funcion(): ##Definicion de la funcion
+    print("Hola, soy una funcion") ##imprime el mensaje
+mi_primer_funcion() ##llama a la funcion para que se ejecute
