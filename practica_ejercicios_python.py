956b3a15ed7097bc16e197707c21613a34b81db3
Martin <mario.terragno@hotmail.com>
2025-04-27 02:27:55 -0300

    Update practica_ejercicios_python.py

--- practica_ejercicios_python.py
+++ practica_ejercicios_python.py
@@ -1,5 +1,13 @@
 # Ejercicios Python https://www.youtube.com/watch?v=mNbUR4anPUY
 
+cadena = "Hola Mundo"
+print(len(cadena)) #Muestra la longitud de la cadena
+
+numero = [20, 32, 80, 90]
+promedio= sum (numero) / len(numero)
+print(promedio) #Suma todos los elementos de la lista y lo divide entre la longitud de la lista
+
+
 numero_uno = 120
 print(numero_uno)
 
@@ -15,4 +23,16 @@ division = numero_uno / numero_dos
 print(division)
 print(multiplicacion)
 print(resta)
+
+
+nombre = "Martin"
+print(nombre)
+
+precio = 15010.50
+descuento = 0.10
+print(precio)
+
+precio_con_descuento = precio - (precio * descuento)
+print(precio_con_descuento)
+
 
