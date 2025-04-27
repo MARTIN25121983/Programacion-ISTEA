da1621b150e42eacecd725322925a7929e3e0bc4
Martin <mario.terragno@hotmail.com>
2025-04-27 02:10:30 -0300

    Update practica_ejercicios_python.py

--- practica_ejercicios_python.py
+++ practica_ejercicios_python.py
@@ -1,3 +1,18 @@
 # Ejercicios Python https://www.youtube.com/watch?v=mNbUR4anPUY
 
+numero_uno = 120
+print(numero_uno)
+
+numero_dos = 20.20
+print(numero_dos)
+
+suma = numero_uno + numero_dos
+print(suma)
+
+resta = numero_uno - numero_dos
+multiplicacion = numero_uno * numero_dos
+division = numero_uno / numero_dos
+print(division)
+print(multiplicacion)
+print(resta)
 
