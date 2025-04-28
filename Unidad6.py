3cab723bde2c1e88369f5f49549a3c3fa4855871
Martin <mario.terragno@hotmail.com>
2025-04-27 21:05:20 -0300

    Update Unidad6.py

--- Unidad6.py
+++ Unidad6.py
@@ -70,3 +70,11 @@ def sumar_todos(*numeros): # se define la funcion y al agregar * se permite vari
 print(sumar_todos(1, 2, 3, 4, 5))  # Imprimirá 15 
 print(sumar_todos(10, 20))  # Imprimirá 30 
 
+#`*args` indica que la función puede recibir cualquier cantidad de valores y los agrupa en una tupla. 
+##Para recibir argumentos con nombre, usamos `**kwargs`
+
+def mostrar_info(**datos):  # 
+    for clave, valor in datos.items(): 
+        print(clave, valor) 
+  
+mostrar_info(nombre="Juan", edad=25, ciudad="Madrid")
