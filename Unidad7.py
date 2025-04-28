e2aabeb9f8e1d39bd8114e75fc4173e7029cd275
Martin <mario.terragno@hotmail.com>
2025-04-27 21:43:55 -0300

    Update Unidad7.py

--- Unidad7.py
+++ Unidad7.py
@@ -1,4 +1,25 @@
 ##Unidad 7 Listas 
 
 ##Una lista es un tipo de dato en Python que nos permite almacenar una colección de elementos en un orden específico
+#Ejemplo de lista 
+#puden tener diferentes tipos de datos int, float, string, boal, etc
+
+colores = ["rojo", "verde", "azul", "amarillo"] 
+print(colores) 
+
+#Funcion len contar la cantidad de elementos de una lista
+
+print("La cantidad de colores son:", len(colores))
+#Acceder a un elemento de la lista. Si queres acceder al primero debemos:
+print(colores[0]) #primer elemento
+
+#Para acceder al ultimo elemento de la lista o empezar del ultimo hacia el primero se usa el numero negativo
+
+numeros= [1,2,3,4,5,6,7,8,9]
+#para acceder al utlimos debemos elegir de la siguiente manera
+print(numeros[-1]) # se accedea al 9
+print(numeros[-2]) # se accedea al 8
+
+
+
 
