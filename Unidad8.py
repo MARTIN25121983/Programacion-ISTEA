24e36319f670ea80e4acbaddf20a4e71cdbb1017
Martin <mario.terragno@hotmail.com>
2025-05-12 19:25:55 -0300

    Update Unidad8.py

---
+++ Unidad8.py
@@ -0,0 +1,7 @@
+#Tuplas - Diccionario
+#Tuplas  Se define con () 
+colores =("rojo", "verde", "azul", "amarillo", "naranja") #Definir tupla
+print(f"El tipo de dato de colores es: {type(colores)}") #Nos indica el tipo de datos
+
+for letra in colores:
+    print(colores)
