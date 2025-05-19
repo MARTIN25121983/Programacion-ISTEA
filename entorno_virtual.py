83e4af54a764eb295e974e6ea6c57f64edfa59d8
Martin <mario.terragno@hotmail.com>
2025-05-19 20:16:50 -0300

    Update entorno_virtual.py

--- entorno_virtual.py
+++ entorno_virtual.py
@@ -5,6 +5,19 @@
 ##pip install requests #Gestor para instalar paquetes
 #Entorno virtual se creo en C:\Users\mario\mi_entorno
 
+## pypi.org se busca modulos
 
+#doc stream
+import nmap
 
+scanner = nmap.PortScanner()
 
+ip = '192.168.0.14'
+puertos = '22-80'  # Puedes cambiar esto a '1-1024' o lo que necesites
+
+scanner.scan(ip, puertos)
+
+print(f"Escaneo de {ip} en puertos {puertos}:")
+for port in scanner[ip]['tcp']:
+    estado = scanner[ip]['tcp'][port]['state']
+    print(f"Puerto {port}/tcp: {estado}")
