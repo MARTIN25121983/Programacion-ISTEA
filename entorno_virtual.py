78ea3756ec2f4e8917fb885df52ad2b89f3c3ce4
Martin <mario.terragno@hotmail.com>
2025-05-19 19:47:41 -0300

    Update entorno_virtual.py

--- entorno_virtual.py
+++ entorno_virtual.py
@@ -1,4 +1,9 @@
 #Creacion de entorno virtual
+py --version #Desde el cmd
 pyhton -m venv mi_entorno #Crear entorno
-source mi_entorno/bin/activate #Activar entorno
+
+mi_entorno\Scripts\activate #Activar entorno
 pip install requests #Gestor para instalar paquetes
+#Entorno virtual se creo en C:\Users\mario\mi_entorno
+
+
