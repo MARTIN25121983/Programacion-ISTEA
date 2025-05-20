2251200603639e4ac740b64a8ad1392212c1257b
Martin <mario.terragno@hotmail.com>
2025-05-20 00:22:32 -0300

    Update entorno_virtual.py

--- entorno_virtual.py
+++ entorno_virtual.py
@@ -8,65 +8,39 @@
 
 import nmap
 
-scanner = nmap.PortScanner()
+# Predefined
+separator = "********************************************"
 
-ip = '192.168.0.14'
-puertos = '22-80'  # Puedes cambiar esto a '1-1024' o lo que necesites
+# Functions
+def printFoundHosts( allHostsList : list[str] ):
+    if len( allHostsList ) > 0:
+        print( "\n*--- Dispositivos encontrados:\n" )
 
-scanner.scan(ip, puertos)
+        for i, host in enumerate( allHostsList ):
+            print( separator )
+            print( f"*** Dispositivo #{ i + 1 } ***" )
+            print( f"Nombre:        { devScanner[host].hostname() }" )
+            print( f"Dirección Ip:  { host }" )
 
-print(f"Escaneo de {ip} en puertos {puertos}:")
-for port in scanner[ip]['tcp']:
-    estado = scanner[ip]['tcp'][port]['state']
-    print(f"Puerto {port}/tcp: {estado}")
+            if 'mac' in devScanner[host]['addresses']:
+                print( f"Dirección MAC: { devScanner[host]['addresses']['mac'] }" )
+
+            print( f"Estado:        { devScanner[host].state() }" )
+            
+            protocolsList = devScanner[host].all_protocols()
+
+            if len( protocolsList ) > 0:
+                print( "\n* Protocolos *" )
+
+                for proto in devScanner[host].all_protocols():
+                    print( f"\tProtocolo: { proto }" )
+                    lport = devScanner[host][proto].keys()
+                    #lport.sort()
+                    for port in lport:
+                        print ( f"\t\tPuerto: { port }\tEstado: { devScanner[host][proto][port]['state'] }" )
+
+        print( separator )
+    else:
+        print( "\nNo se encontraron dispositivos en el segmento de red indicado." )
 
 ## pypi.org se busca modulos
-
-def calculadora(operacion, num1, num2):
-    """
-    Operacion matematica entre dos números.
-
-    Parámetros:
-        operacion (str): Tipo de operación: "suma", "resta", "multiplicación", "división".
-        num1 (float): Primer número.
-        num2 (float): Segundo número.
-
-    Retorna:
-        float: Resultado de la operación.
-
-    Maneja errores comunes como:
-        - Operación no válida
-    """
-    try:
-        if operacion == "suma":
-            return num1 + num2
-        elif operacion == "resta":
-            return num1 - num2
-        elif operacion == "multiplicación":
-            return num1 * num2
-        elif operacion == "división":
-            return num1 / num2
-        else:
-            ValueError("Operación no válida. Usa: suma, resta, multiplicación o división.")
-    except ZeroDivisionError:
-        return "Error: No se puede dividir entre cero."
-
-
-#doc stream
-"""import nmap
-
-scanner = nmap.PortScanner()
-
-ip = '192.168.0.14'
-puertos = '22-80'  # Puedes cambiar esto a '1-1024' o lo que necesites
-
-scanner.scan(ip, puertos)
-
-print(f"Escaneo de {ip} en puertos {puertos}:")
-for port in scanner[ip]['tcp']:
-    estado = scanner[ip]['tcp'][port]['state']
-    print(f"Puerto {port}/tcp: {estado}")
-
-
-
-"""
