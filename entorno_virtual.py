0c6d6822a011007f827a094ea8e03e97915eff14
Martin <mario.terragno@hotmail.com>
2025-05-19 21:06:55 -0300

    clase

--- entorno_virtual.py
+++ entorno_virtual.py
@@ -1,25 +1,10 @@
 #Creacion de entorno virtual
 ##py --version #Desde el cmd
-
-import nmap
-
-scanner = nmap.PortScanner()
-
-ip = '192.168.0.14'
-puertos = '22-80'  # Puedes cambiar esto a '1-1024' o lo que necesites
-
-scanner.scan(ip, puertos)
-
-print(f"Escaneo de {ip} en puertos {puertos}:")
-for port in scanner[ip]['tcp']:
-    estado = scanner[ip]['tcp'][port]['state']
-    print(f"Puerto {port}/tcp: {estado}")
-
 ##pyhton -m venv mi_entorno #Crear entorno
-##mi_entorno\Scripts\activate #Activar entorno
+##mi_entorno_virtual\Scripts\activate.bat #Activar entorno
 ##pip install requests #Gestor para instalar paquetes
-#Entorno virtual se creo en C:\Users\mario\mi_entorno
-
+#Entorno virtual se creo en C:\Users\mario\mi_entorno_virtual
+##https://www.programaenpython.com/miscelanea/crear-entornos-virtuales-en-python/
 
 
 ## pypi.org se busca modulos
